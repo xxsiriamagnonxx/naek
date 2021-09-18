ccae7b1184cd28dc8a4f1ffc9dc61872c879b0af
xxsiriamagnonxx <90441349+xxsiriamagnonxx@users.noreply.github.com>
2021-09-18 19:25:52 +0200

    Add files via upload

--- VENOM.lua
+++ VENOM.lua
@@ -3824,7 +3824,7 @@ keyboard.inline_keyboard = {
 {text = 'مطور ثانوي', callback_data='amr@'..msg.sender_user_id_..'/user@'..result.id_.."/SecondSudo"},
 }, 
 {
-local msg_id = msg.id_/2097152/0.5
+Local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 if text == 'اصنع يوزر' or text == 'اصنع' then
