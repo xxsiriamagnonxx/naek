e1218e6b0a96b91dd66a79a8d4cbea76d0af9464
xxsiriamagnonxx <90441349+xxsiriamagnonxx@users.noreply.github.com>
2021-09-21 07:19:29 +0200

    Add files via upload

--- VENOM.lua
+++ VENOM.lua
@@ -17093,6 +17093,7 @@ bot_data:sadd(ban_id..'Sudo:User', userid)
 return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(text)..'&message_id='..msg_idd) 
 tdcli_function ({ID = "GetUser",user_id_ = userid},function(arg,data) 
 return false 
+end,nil)
 end
 if data.first_name_ then
 usertext = '\n ᥀ الـعـضو   ⋙ ['..data.first_name_..'](t.me/'..(data.username_ or 'textchuser')..')'
