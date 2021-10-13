b94b8f552faa1e5ffb2ea3ebb3112297f5960f6f
xxsiriamagnonxx <90441349+xxsiriamagnonxx@users.noreply.github.com>
2021-10-14 01:39:43 +0200

    Add files via upload

--- ANUBIS.lua
+++ ANUBIS.lua
@@ -1866,7 +1866,59 @@ local Key = {
 send_inline_key(msg.chat_id_,Text,Key)
 end 
 end
-
+if text == '•━═══『⚜قسم السورس⚜』═══━•' then
+local Text = 'مرحبا بك في قسم السورس'
+local Key = {
+{'عايز بوت'},
+{'همس'},
+{'𝙼𝙰𝚈 𝙱𝙾𝚃𝚂 𖣴'},
+{'العوده🍂'},
+}
+send_inline_key(msg.chat_id_,Text,Key)
+end 
+end
+if text == '•━═══『⚜قسم البوتات⚜』═══━•' then
+local Text = 'هنا ستجد قناه مختصه بكل بوتات المبرمج 😍'
+local Key = {
+{'همس'},
+{'𝙼𝙰𝚈 𝙱𝙾𝚃𝚂 𖣴'},
+{'العوده🍂'},
+}
+send_inline_key(msg.chat_id_,Text,Key)
+end 
+end
+if text == '•━═══『⚜قسم المميزات⚜』═══━•' then
+local Text = 'مميزات خاصه ب اللي منصبين مميزات '
+local Key = {
+{'•━═══『⚜𝙷𝙼𝚂⚜』═══━•'},
+{'اغاني','مميزات'},
+{'•━═══『⚜𝙷𝙼𝚂⚜』═══━•'},
+{'الافلام','العاب'},
+{'•━═══『⚜𝙷𝙼𝚂⚜』═══━•'},
+{'قران','روايات'},
+{'•━═══『⚜𝙷𝙼𝚂⚜』═══━•'},
+{'استوري'},
+{'•━═══『⚜𝙷𝙼𝚂⚜』═══━•'},
+{'كشف الكذب','الترجمه'},
+{'𝔟𝔞𝔠𝔨 ⚡'},
+}
+send_inline_key(msg.chat_id_,Text,Key)
+end 
+end
+if text == '•━═══『⚜قسم التواصل⚜』═══━•' then
+local Text = [[ 
+قناه مطورين السورس 🍂
+محتاج تنصب بوت حمايه ببلاش تواصل معانا✅
+]]
+keyboard = {}  
+keyboard.inline_keyboard = { 
+{{text = 'مطورين السورس⚙', url="t.me/hms_00"}}, 
+{{text = 'بوت التواصل', url="t.me/Towasolbot"}}, 
+} 
+local msg_id = msg.id_/2097152/0.5 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/vagwg/8&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end
+end
 if text and text:match("^/start ph(.*)$") then
 Sf = text:match("^/start ph(.*)$")
 local list = bot_data:smembers(ban_id.."filterphoto"..Sf)  
@@ -2764,7 +2816,7 @@ local hms_00 = bot_data:get(ban_id..'text:ch:user')
 if hms_00 then
 send(msg.chat_id_, msg.id_,'['..hms_00..']')
 else
-send(msg.chat_id_, msg.id_,' 𖢅⤈  لا تستطيع استخدام البوت \n 𖢅⤈   يرجى الاشتراك بالقناه اولا \n 𖢅⤈   اشترك هنا ['..bot_data:get(ban_id..'add:ch:username')..']')
+send(msg.chat_id_, msg.id_,' 𖢅⤈  لا تستطيع استخدام البوت \n 𖢅⤈   يرجى الاشتراك بالقناه اولا \n ??⤈   اشترك هنا ['..bot_data:get(ban_id..'add:ch:username')..']')
 end
 return false
 end
@@ -5635,7 +5687,7 @@ return false
 end
 if text == ("مسح الحظر العام") and Devban(msg) then
 bot_data:del(ban_id..'GDRG:User')
-send(msg.chat_id_, msg.id_, '\n 𖢅⤈  تم مسح الحظر العام')
+send(msg.chat_id_, msg.id_, '\n ??⤈  تم مسح الحظر العام')
 return false
 end
 if text == ("مسح الكتم العام") and Devban(msg) then
@@ -9785,7 +9837,7 @@ local hms_00 = bot_data:get(ban_id..'text:ch:user')
 if hms_00 then
 send(msg.chat_id_, msg.id_,'['..hms_00..']')
 else
-send(msg.chat_id_, msg.id_,' ??⤈  لا تستطيع استخدام البوت \n 𖢅⤈   يرجى الاشتراك بالقناه اولا \n 𖢅⤈   اشترك هنا ['..bot_data:get(ban_id..'add:ch:username')..']')
+send(msg.chat_id_, msg.id_,' 𖢅⤈  لا تستطيع استخدام البوت \n 𖢅⤈   يرجى الاشتراك بالقناه اولا \n 𖢅⤈   اشترك هنا ['..bot_data:get(ban_id..'add:ch:username')..']')
 end
 return false
 end
@@ -18615,7 +18667,7 @@ local Teext =[[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
-{text = '𖢅⤈ 𝙱𝙰𝙲𝙺↵', callback_data="/help8"},
+{text = '??⤈ 𝙱𝙰𝙲𝙺↵', callback_data="/help8"},
 },
 {
 {text ='⌯اخفاء الكليشه⌯ ', callback_data="/hide"},
